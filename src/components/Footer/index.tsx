d64c5d20b35b8cfca83426da2566413318575301
Azamat <azamatshamshiev18@gmail.com>
2024-09-26 16:25:20 +0300

    update instagram nickname

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -19,7 +19,7 @@ const Footer = () => {
                         <div className="footer--title__icons">
                           <div>
                               <p>Me at WebSites</p>
-                              <Link  className="footer--title__icons--insta" to="https://www.instagram.com/llazamaaat" >
+                              <Link  className="footer--title__icons--insta" to="https://www.instagram.com/llazamaaat">
                                   <BsInstagram />
                               </Link>
                               <Link className="footer--title__icons--github" to={"https://www.github.com/shamshievv"}>
