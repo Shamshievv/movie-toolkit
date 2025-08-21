2021fd06ed2a3240ebb5ccc89e652c07eb1eb7ea
Azamat <azamatshamshiev18@gmail.com>
2025-08-21 12:21:49 +0600

    inst update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.scss';
 import Popular from "./pages/Popular";
-import Header from "./components/Header";
+import Header from"./components/Header";
 import {Route, Routes} from "react-router-dom";
 import TopRated from "./pages/TopRated";
 import Details from "./pages/DetailPage";
